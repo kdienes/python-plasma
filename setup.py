3ec72cd3bd8fe3c75032a3e6ee4b6f03ccd3927e
Klee Dienes <klee.dienes@hadronindustries.com>
2012-08-30 09:58:25 -0400

    Move python config to top-level.

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ import os.path
 import sys
 import distutils.core
 
-import vars
+sys.path = [ '..' ] + sys.path
+import config
 
 if os.name == 'posix':
     CXX_libraries = [ 'stdc++', 'm' ]
@@ -18,8 +19,8 @@ if sys.platform != "win32":
 plasma = distutils.core.Extension \
 ("libplasma",
  [ "plasma.c" ],
- include_dirs = [ os.path.join (vars.G_SPEAK_DIR, 'include') ],
- library_dirs= [ os.path.join (vars.G_SPEAK_DIR, 'lib') ],
+ include_dirs = [ os.path.join (config.G_SPEAK_DIR, 'include') ],
+ library_dirs= [ os.path.join (config.G_SPEAK_DIR, 'lib') ],
  libraries = [ "Plasma","Loam","Basement","Afferent","Ganglia","Impetus"],
  extra_compile_args = [ '-Wall', '-g', '-O0']
  )
