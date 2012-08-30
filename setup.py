4ccded1ea2c58c4ecc0eb0a911a4026db656a9a0
Klee Dienes <klee.dienes@hadronindustries.com>
2012-08-30 14:02:43 -0700

    moving stuff for packaging

--- setup.py
+++ setup.py
@@ -7,31 +7,26 @@ import distutils.core
 
 import config
 
-if os.name == 'posix':
-    CXX_libraries = [ 'stdc++', 'm' ]
-else:
-    CXX_libraries = []
-
 if sys.platform != "win32":
     compiler = [ '-Wall -Werror -fpermissive -Wno-strict-prototypes' ]
 
-plasma = distutils.core.Extension \
-("libplasma",
- [ "plasma.c" ],
+libplasma = distutils.core.Extension \
+('libplasma',
+ [ 'plasma.c' ],
  include_dirs = [ os.path.join (config.G_SPEAK_DIR, 'include') ],
- library_dirs= [ os.path.join (config.G_SPEAK_DIR, 'lib') ],
+ library_dirs = [ os.path.join (config.G_SPEAK_DIR, 'lib') ],
+ runtime_library_dirs = [ os.path.join (config.G_SPEAK_DIR, 'lib') ],
  libraries = [ 'Plasma' ],
  extra_compile_args = [ '-Wall', '-g', '-O0']
  )
 
 distutils.core.setup \
-(name = "libPlasma module",
- version = "0.1",
- maintainer = "Klee Dienes",
- maintainer_email = "klee.dienes@hadronindustries.com",
- description = "Utilities for interfacing to g-speak libPlasma.",
- packages = [ ],
- package_dir = { },
- url = "http://www.hadronindustries.com",
- ext_modules = [ plasma ],
+(name = 'python-plasma',
+ version = '0.0.1',
+ maintainer = 'Klee Dienes',
+ maintainer_email = 'klee.dienes@hadronindustries.com',
+ description = 'Utilities for interfacing to g-speak libPlasma.',
+ packages = [ 'plasma' ],
+ url = 'http://www.hadronindustries.com',
+ ext_modules = [ libplasma ],
  )
